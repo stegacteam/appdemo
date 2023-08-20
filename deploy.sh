c90e4fb4ec1667588e3f31e54086c65b02115c35
Huiting <huitingyeong75@gmail.com>
2023-08-20 20:17:47 +0800

    add

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+gcloud container clusters get-credentials innotech-demo-gke --region asia-southeast1 --project innotech-demo
+
+kubectl create namespace market-demo
+
+kubectl apply -f api-deployment.yaml
+
+sleep 20
+
+kubectl apply -f api-ingress.yaml
+
+sleep 30
+
+kubectl apply -f api-ingress.yaml
+
+kubectl apply -f web-deployment.yaml
+
+sleep 20
+
+kubectl apply -f web-service.yaml
