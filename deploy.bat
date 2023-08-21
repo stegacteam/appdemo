a30b24f0069258169ec287fb9630e3bb4fdbe983
Hui-WorkPC <huitingyeong75@gmail.com>
2023-08-21 12:05:03 +0800

    add

--- deploy.bat
+++ deploy.bat
@@ -2,3 +2,5 @@
 
 rem
 for /f "delims=" %%i in ('gcloud container clusters get-credentials innotech-demo-gke-h --region asia-southeast1 --project innotech-demo') do set "output=%%i"
+
+echo The output of dir command is: %output%
