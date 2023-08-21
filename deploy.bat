8ae47b387cb9ad31a5e551252cb79e152d123cec
Hui-WorkPC <huitingyeong75@gmail.com>
2023-08-21 12:08:33 +0800

    add

--- deploy.bat
+++ deploy.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal enabledelayedexpansion
+@REM setlocal enabledelayedexpansion
 
 rem
 for /f "delims=" %%i in ('gcloud container clusters get-credentials innotech-demo-gke-h --region asia-southeast1 --project innotech-demo') do set "output=%%i"
