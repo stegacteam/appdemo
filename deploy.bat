eb6d7a391f8dd8c1f3831033792b8b28f48c9129
Hui-WorkPC <huitingyeong75@gmail.com>
2023-08-21 12:05:46 +0800

    add

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,8 @@
 @echo off
+setlocal enabledelayedexpansion
 
 rem
 for /f "delims=" %%i in ('gcloud container clusters get-credentials innotech-demo-gke-h --region asia-southeast1 --project innotech-demo') do set "output=%%i"
 
 echo The output of dir command is: %output%
+endlocal
