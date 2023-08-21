8880201995cca54c9e5baf630f2bff80ab4d5bf7
Hui-WorkPC <huitingyeong75@gmail.com>
2023-08-21 11:59:39 +0800

    add

--- deploy.bat
+++ deploy.bat
@@ -1,2 +1,4 @@
-result=$(gcloud container clusters get-credentials innotech-demo-gke-h --region asia-southeast1 --project innotech-demo)
-echo $result
+@echo off
+
+rem
+gcloud container clusters get-credentials innotech-demo-gke-h --region asia-southeast1 --project innotech-demo
