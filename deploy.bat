1c2b6da92e26957d213c46939e751759f7b6ec87
Hui-WorkPC <huitingyeong75@gmail.com>
2023-08-21 12:08:28 +0800

    add

--- deploy.bat
+++ deploy.bat
@@ -2,9 +2,6 @@
 setlocal enabledelayedexpansion
 
 rem
-for /f "delims=" %%i in ('gcloud container clusters get-credentials innotech-demo-gke-h --region asia-southeast1 --project innotech-demo') do (
-  set "output=%%i"
-)
+for /f "delims=" %%i in ('gcloud container clusters get-credentials innotech-demo-gke-h --region asia-southeast1 --project innotech-demo') do set "output=%%i"
 
-@REM echo The output is: !output!
 endlocal
